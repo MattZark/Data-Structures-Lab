5d5d4147521c14a6c5be193a2741c6edeb4847b3
MattZark <145050769+MattZark@users.noreply.github.com>
2025-02-26 11:32:54 -0600

    Create Lab_1.c

---
+++ Lab_1.c
@@ -0,0 +1,70 @@
+//Remove Linked List Elements
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode dummy;
+    dummy.next = head;
+
+    struct ListNode* prev = &dummy;
+    struct ListNode* current = head;
+    
+    while (current != NULL) {
+        if (current->val == val) {
+            prev->next = current->next;
+        } else {
+            prev = current;
+        }
+        current = current->next;  
+    }
+
+    return dummy.next;
+}
+
+
+
+//Reverse Linked List
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+ 
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+
+    while (curr != NULL) {
+        struct ListNode* nextTemp = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
+
+struct ListNode* createNode(int val) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void printList(struct ListNode* head) {
+    struct ListNode* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->val);
+        temp = temp->next;
+    }
+    printf("\n");
+}
